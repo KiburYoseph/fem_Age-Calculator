e5f93bf7968ced4bf74e85551a7fed4aef6e114c
Kibur_Yoseph <kiburstgr@gmail.com>
2023-04-18 09:09:45 +0300

    age operator functional

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
-/*const button = document.getElementById("button")
+const d = new Date();
+const currentYear = d.getFullYear();
+const currentMonth = (d.getMonth()) + 1;
+const currentDay = d.getDate();
 
-function valueCheck(){
+const button = document.getElementById("button")
+const dayInput = document.getElementById("dayInput");
+const monthInput = document.getElementById("monthInput");
+const yearInput = document.getElementById("yearInput");
+const yearOutput = document.getElementById("yearOutput");
+const monthOutput = document.getElementById("monthOutput");
+const dayOutput = document.getElementById("dayOutput");
+
+function operate () {
+    let userDay = dayInput.value;
+    let userMonth = monthInput.value;
+    let userYear = yearInput.value;
+    let dayResult = Math.abs((userDay - currentDay));
+    let monthResult = Math.abs((userMonth - currentMonth));
+    let yearResult = Math.abs((userYear - currentYear));
     
+    yearOutput.innerHTML = yearResult;
+    monthOutput.innerHTML = monthResult;
+    dayOutput.innerHTML = dayResult;
 }
 
+
+
 button.onclick = function() {
-    let correctInput;
+    operate();
 
 
-
-}*/
-
+}
