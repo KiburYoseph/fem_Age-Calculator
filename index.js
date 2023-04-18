e9bacc8b3c2d3cb5d5d6ddd55107084162a303e1
Kibur_Yoseph <kiburstgr@gmail.com>
2023-04-18 09:54:36 +0300

    Input Check Functional

--- index.js
+++ index.js
@@ -10,8 +10,11 @@ const yearInput = document.getElementById("yearInput");
 const yearOutput = document.getElementById("yearOutput");
 const monthOutput = document.getElementById("monthOutput");
 const dayOutput = document.getElementById("dayOutput");
+const dayError = document.querySelector(".dayError");
+const monthError = document.querySelector(".monthError");
+const yearError = document.querySelector(".yearError");
 
-function operate () {
+function operate() {
     let userDay = dayInput.value;
     let userMonth = monthInput.value;
     let userYear = yearInput.value;
@@ -24,10 +27,56 @@ function operate () {
     dayOutput.innerHTML = dayResult;
 }
 
+function checkInputs() {
+    let userDay = dayInput.value;
+    let userMonth = monthInput.value;
+    let userYear = yearInput.value;
+    let correct = "true";
+    
+    if(userDay == ""){
+        dayError.innerHTML = "Please Enter A Value";
+        correct = "false";
+    }
+    else if (userDay > 31 || userDay <= 0){
+        dayError.innerHTML = "Please Enter A Valid Input";
+        correct = "false";
+    }
+    
+    if(userMonth == ""){
+        monthError.innerHTML = "Please Enter A Value";
+        correct = "false";
+    }
+    else if (userMonth > 12 || userMonth <= 0){
+        monthError.innerHTML = "Please Enter A Valid Input";
+        correct = "false";
+    } 
+    
+    if(userYear == ""){
+        yearError.innerHTML = "Please Enter A Value";
+        correct = "false";
+    }
+    else if(userYear > currentYear || userYear < 0){
+        yearError.innerHTML = "Please Enter A Valid Input";
+        correct = "false";
+    }
+    
+
+    return correct;
+}
 
 
 button.onclick = function() {
-    operate();
-
+    yearError.innerHTML = "";
+    monthError.innerHTML = "";
+    dayError.innerHTML = "";
+    
+    check = checkInputs();
+    
+    if(check == "true"){
+        operate();
+    }
+    else{
+        return null;
+    }
 
 }
